9f43939fb857cf404a37124c9e4a2b8b838715d7
Amna <amnae94@gmail.com>
2020-08-02 21:50:17 +0100

    Completed follow-through part of tutorial.

--- src/index.js
+++ src/index.js
@@ -11,53 +11,19 @@ function Square(props) {
     );
 }
 
-class Board extends React.Component {
-  constructor(props) {
-      super(props);
-      //squares details in board not in individual squares
-      //To collect data from multiple children, or to have two child components communicate with each other, you need to declare the shared state in their parent component instead. The parent component can pass the state back down to the children by using props; this keeps the child components in sync with each other and with the parent component.
-      this.state = {
-        squares: Array(9).fill(null),
-        xIsNext: true
-      };
-  }
-  
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    //if someone has won or that square has already been picked, do nothing
-    if(calculateWinner(this.state.squares) || squares[i]) {
-        return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext
-    });
-  }
-  
+class Board extends React.Component {  
   renderSquare(i) {
     return (
       <Square 
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    
-    //if someone has won, tell them, otherwise say whose turn it is
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    }else{
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-    
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -79,15 +45,80 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      stepNumber: 0,
+      xIsNext: true,
+    };
+  }
+  
+  handleClick(i) {
+    const history = this.state.history.slice(0,this.state.stepNumber+1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    //if someone has won or that square has already been picked, do nothing
+    if(calculateWinner(squares) || squares[i]) {
+        return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares:squares
+      }]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+  
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step%2) ===0,
+    });
+  }
+  
+  
   render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ? 
+        'Go to move #' + move:
+        'Go to game start';
+      return(
+        <li key = {move}>
+          <button onClick = {() => this.jumpTo(move)}> {desc} </button>
+        </li>
+      )
+    });    
+
+    //if someone has won, tell them, otherwise say whose turn it is
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    }else{
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+    
+
+              
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board 
+            squares = {current.squares}
+            onClick = {(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
